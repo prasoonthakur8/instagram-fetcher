c624aa0eb09b98cde98ecc7137e03763e59091ee
Anurag Deep <anuragdeep.xon@gmail.com>
2023-04-19 06:37:07 +0530

    Graph API added

--- test.js
+++ test.js
@@ -2,14 +2,22 @@ const InstaFetcherPro = require('./lib/fetcher');
 
 (async () => {
   const fetcher = new InstaFetcherPro();
-  const posts = await fetcher.getPostsByUsername('instagram');
+
+
+  const posts = await fetcher.fetchData('instagram');
+
+  console.log(posts)
+
+  return posts
+
+  // const posts = await fetcher.getPostsByUsername('instagram');
   
-  for (const post of posts) {
-    console.log(`ID: ${post.id}`);
-    console.log(`Type: ${post.type}`);
-    console.log(`Media URL(s): ${post.mediaUrl}`);
-    console.log(`Caption: ${post.caption}`);
-    console.log(`Timestamp: ${post.timestamp}`);
-    console.log('-------------------------');
-  }
+  // for (const post of posts) {
+  //   console.log(`ID: ${post.id}`);
+  //   console.log(`Type: ${post.type}`);
+  //   console.log(`Media URL(s): ${post.mediaUrl}`);
+  //   console.log(`Caption: ${post.caption}`);
+  //   console.log(`Timestamp: ${post.timestamp}`);
+  //   console.log('-------------------------');
+  // }
 })();
